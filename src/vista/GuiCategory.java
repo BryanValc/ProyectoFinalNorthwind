9eb773a461cd82e4f056355876c73a59f0eea12b
BryanValc <bryan501th@gmail.com>
2022-05-09 20:31:42 -0500

    Se terminó de agregar las validaciones necesarias en GuiCategory

--- src/vista/GuiCategory.java
+++ src/vista/GuiCategory.java
@@ -25,7 +25,7 @@ public class GuiCategory extends JFrame {
 	private JPanel contentPane;
 	private JTable table;
 	private JScrollPane scrollPane;
-	private JButton btnLimpiar,btnOperacion;
+	private JButton btnLimpiar, btnOperacion;
 	JComboBox<String> comboOperacion, comboFiltro;
 
 	/**
@@ -75,7 +75,7 @@ public class GuiCategory extends JFrame {
 		JLabel lblDescripcin = new JLabel("Descripci\u00F3n");
 		lblDescripcin.setBounds(10, 61, 91, 14);
 		contentPane.add(lblDescripcin);
-		
+
 		btnLimpiar = new JButton("Limpiar");
 		btnLimpiar.setToolTipText("Limpiar el formulario");
 		btnLimpiar.addActionListener(new ActionListener() {
@@ -85,56 +85,73 @@ public class GuiCategory extends JFrame {
 		});
 		btnLimpiar.setBounds(10, 85, 89, 23);
 		contentPane.add(btnLimpiar);
-		
+
 		comboFiltro = new JComboBox();
-		comboFiltro.setToolTipText("Selecciona el tipo de busqueda, la b\u00FAsqueda amplia busca cualquier coincidencia en cualquier campo, la b\u00FAsqueda precisa busca que todos los campos coincidan");
-		comboFiltro.setModel(new DefaultComboBoxModel(new String[] {"B\u00FAsqueda amplia", "B\u00FAsqueda precisa"}));
+		comboFiltro.setToolTipText(
+				"Selecciona el tipo de busqueda, la b\u00FAsqueda amplia busca cualquier coincidencia en cualquier campo, la b\u00FAsqueda precisa busca que todos los campos coincidan");
+		comboFiltro
+				.setModel(new DefaultComboBoxModel(new String[] { "B\u00FAsqueda amplia", "B\u00FAsqueda precisa" }));
 		comboFiltro.setBounds(298, 7, 126, 22);
 		contentPane.add(comboFiltro);
-		
+
 		comboOperacion = new JComboBox();
-		comboOperacion.setModel(new DefaultComboBoxModel(new String[] {"Insertar", "Modificar", "Borrar"}));
+		comboOperacion.setModel(new DefaultComboBoxModel(new String[] { "Insertar", "Modificar", "Borrar" }));
 		comboOperacion.setBounds(298, 32, 126, 22);
 		comboOperacion.setToolTipText("Selecciona el tipo de operaci�n");
-        comboOperacion.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                comboOperacionActionPerformed(evt);
-            }
-        });
+		comboOperacion.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				comboOperacionActionPerformed(evt);
+			}
+		});
 		contentPane.add(comboOperacion);
-		
+
 		btnOperacion = new JButton("Insertar");
 		btnOperacion.setBounds(298, 57, 89, 23);
 		contentPane.add(btnOperacion);
-		
+
 		JButton btnAplicar = new JButton("Aplicar");
 		btnAplicar.setToolTipText("Aplicar los cambios realizados a la base de datos");
 		btnAplicar.setBounds(298, 85, 89, 23);
 		contentPane.add(btnAplicar);
-		
+
 		caja1 = new JTextField();
 		caja1.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                caja1KeyPressed(evt);
-            }
-            public void keyReleased(java.awt.event.KeyEvent evt) {
-                caja1KeyReleased(evt);
-            }
-        });
+			public void keyPressed(java.awt.event.KeyEvent evt) {
+				caja1KeyPressed(evt);
+			}
+
+			public void keyReleased(java.awt.event.KeyEvent evt) {
+				caja1KeyReleased(evt);
+			}
+		});
 		caja1.setBounds(108, 8, 86, 20);
 		contentPane.add(caja1);
 		caja1.setColumns(10);
-		
+
 		caja2 = new JTextField();
+		caja2.addKeyListener(new java.awt.event.KeyAdapter() {
+			public void keyPressed(java.awt.event.KeyEvent evt) {
+				caja2KeyPressed(evt);
+			}
+
+			public void keyReleased(java.awt.event.KeyEvent evt) {
+				caja2KeyReleased(evt);
+			}
+		});
 		caja2.setColumns(10);
 		caja2.setBounds(108, 33, 126, 20);
 		contentPane.add(caja2);
-		
+
 		scrollPane_1 = new JScrollPane();
 		scrollPane_1.setBounds(111, 61, 161, 52);
 		contentPane.add(scrollPane_1);
-		
+
 		caja3 = new JTextArea();
+		caja3.addKeyListener(new java.awt.event.KeyAdapter() {
+			public void keyReleased(java.awt.event.KeyEvent evt) {
+				caja3KeyReleased(evt);
+			}
+		});
 		scrollPane_1.setViewportView(caja3);
 	}
 
@@ -174,86 +191,120 @@ public class GuiCategory extends JFrame {
 		caja2.setText("");
 		caja3.setText("");
 	}
-	
-	String op1,op2,op3;
+
+	String op1, op2, op3;
 	private JTextField caja1;
 	private JTextField caja2;
 	private JTextArea caja3;
 	private JScrollPane scrollPane_1;
-    public void setOps(JComboBox<String> caja) {
-		switch (""+caja.getSelectedItem()) {
-		case "B\u00FAsqueda precisa":
-	            op1="= ";
-	            op2=" AND ";
-	            op3="";
-	            break;
-		case "B\u00FAsqueda amplia":
-	            op1="LIKE ";
-	            op2=" OR ";
-	            op3="%";
-	            break;
-		default:
-			break;
+
+	public void setOps(JComboBox<String> caja) {
+		switch ("" + caja.getSelectedItem()) {
+			case "B\u00FAsqueda precisa":
+				op1 = "= ";
+				op2 = " AND ";
+				op3 = "";
+				break;
+			case "B\u00FAsqueda amplia":
+				op1 = "LIKE ";
+				op2 = " OR ";
+				op3 = "%";
+				break;
+			default:
+				break;
 		}
-    }
+	}
 
 	public String consulta() {
 		String sql = "SELECT CategoryID, CategoryName, Description FROM Categories ";
 		setOps(comboFiltro);
-		
-		boolean primero=true;
-		if(!caja1.getText().toString().equals("")) {
-				if (!primero) {sql+=op2;}else {sql+="WHERE ";}
-				primero=false;
-				sql+=("CategoryID "+op1+" '"+op3+caja1.getText().toString()+op3+"'");
-		}
-		if(!btnOperacion.getText().contains("Modificar")){
-			if(!caja2.getText().toString().equals("")) {
-				if (!primero) {sql+=op2;}else {sql+="WHERE ";}
-				primero=false;
-				sql+=("CategoryName "+op1+" '"+op3+caja2.getText().toString()+op3+"'");
+
+		boolean primero = true;
+		if (!caja1.getText().toString().equals("")) {
+			if (!primero) {
+				sql += op2;
+			} else {
+				sql += "WHERE ";
 			}
-			if(!caja3.getText().toString().equals("")) {
-				if (!primero) {sql+=op2;}else {sql+="WHERE ";}
-				primero=false;
-				sql+=("Description "+op1+" '"+op3+caja3.getText().toString()+op3+"'");
+			primero = false;
+			sql += ("CategoryID " + op1 + " '" + op3 + caja1.getText().toString() + op3 + "'");
+		}
+		if (!btnOperacion.getText().contains("Modificar")) {
+			if (!caja2.getText().toString().equals("")) {
+				if (!primero) {
+					sql += op2;
+				} else {
+					sql += "WHERE ";
+				}
+				primero = false;
+				sql += ("CategoryName " + op1 + " '" + op3 + caja2.getText().toString() + op3 + "'");
+			}
+			if (!caja3.getText().toString().equals("")) {
+				if (!primero) {
+					sql += op2;
+				} else {
+					sql += "WHERE ";
+				}
+				primero = false;
+				sql += ("Description " + op1 + " '" + op3 + caja3.getText().toString() + op3 + "'");
 			}
 		}
-		
+
 		System.out.println(sql);
 		return sql;
 	}
-	
-	private void comboOperacionActionPerformed(java.awt.event.ActionEvent evt) {                                               
-        btnOperacion.setText(""+comboOperacion.getSelectedItem());
-    }
+
+	private void comboOperacionActionPerformed(java.awt.event.ActionEvent evt) {
+		btnOperacion.setText("" + comboOperacion.getSelectedItem());
+	}
 
 	private void caja1KeyPressed(java.awt.event.KeyEvent evt) {
-		int code=evt.getKeyCode();
-	    int limite=10;
-	    int valorMaximo =2147483647;
-	        JTextField caja = caja1;
-	    if (((evt.getKeyChar() >= '0'&&evt.getKeyChar() <= '9'))&&caja.getText().length()<limite) {
-	        try {
-				int valorCaja = Integer.parseInt(caja1.getText()+evt.getKeyChar());
-				if (valorCaja<=valorMaximo) {
+		int code = evt.getKeyCode();
+		int limite = 10;
+		int valorMaximo = 2147483647;
+		JTextField caja = caja1;
+		if (((evt.getKeyChar() >= '0' && evt.getKeyChar() <= '9')) && caja.getText().length() < limite) {
+			try {
+				int valorCaja = Integer.parseInt(caja.getText() + evt.getKeyChar());
+				if (valorCaja <= valorMaximo) {
 					caja.setEditable(true);
-				}else {
+				} else {
 					caja.setEditable(false);
 				}
 			} catch (Exception e) {
 				caja.setEditable(false);
 			}
-	    }else if(code==KeyEvent.VK_BACK_SPACE) {
-	    	caja.setEditable(true);
-	    }else{
-	        caja.setEditable(false);
-	    }
-	 }
+		} else if (code == KeyEvent.VK_BACK_SPACE) {
+			caja.setEditable(true);
+		} else {
+			caja.setEditable(false);
+		}
+	}
+	
+	private void caja2KeyPressed(java.awt.event.KeyEvent evt) {
+		int code = evt.getKeyCode();
+        int limite = 15;
+        JTextField caja = caja2;
+        if ((caja.getText().equals("")?true:!(caja.getText().charAt(caja.getText().length()-1)==' '&&code==KeyEvent.VK_SPACE))&&(caja.getText().length()<limite||code==KeyEvent.VK_BACK_SPACE)) {
+                caja.setEditable(true);
+        }else{
+                caja.setEditable(false);
+        }
+	}
 
-	 private void caja1KeyReleased(java.awt.event.KeyEvent evt) {                                  
-        String sql = consulta();
-        actualizarTabla(sql);
-    }
+	private void caja1KeyReleased(java.awt.event.KeyEvent evt) {
+		String sql = consulta();
+		actualizarTabla(sql);
+	}
+	
+	private void caja2KeyReleased(java.awt.event.KeyEvent evt) {
+		String sql = consulta();
+		actualizarTabla(sql);
+	}
+	
+	private void caja3KeyReleased(java.awt.event.KeyEvent evt) {
+		String sql = consulta();
+		actualizarTabla(sql);
+	}
 
 }
